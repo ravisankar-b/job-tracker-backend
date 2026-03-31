3485d874f3748d52dc3e2f40c1f5f67e52ec99dd
Ravisankar <beeramravisankar@gmail.com>
2026-03-31 02:23:49 -0500

    feat: add CORSConfig and update SecurityConfig

--- src/main/java/com/ravi/job_tracker_backend/security/SecurityConfig.java
+++ src/main/java/com/ravi/job_tracker_backend/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.ravi.job_tracker_backend.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -25,7 +26,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain  securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+        http.
+                cors(Customizer.withDefaults()).
+                csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers("/auth/**").permitAll()
                                 .anyRequest().authenticated())
